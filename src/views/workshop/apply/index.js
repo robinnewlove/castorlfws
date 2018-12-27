094cb64105c046e141a5f902296bd6e4198e84f0
Woshiajuana <zhigang.chen@owulia.com>
2018-12-27 19:56:32 +0800

    提交

--- src/views/workshop/apply/index.js
+++ src/views/workshop/apply/index.js
@@ -4,7 +4,27 @@
 // 控制器
 const Controller = {
     init () {
-
+        let eleInput = this.getTagName('input');
+        let eleSelect = this.getTagName('select');
+        let eleTextarea = this.getTagName('textarea');
+        this.addListener(eleInput);
+        this.addListener(eleSelect);
+        this.addListener(eleTextarea);
+    },
+    getTagName (name) {
+        let ele = document.getElementsByTagName(name);
+        return ele ? Array.prototype.slice.apply(ele) : [];
+    },
+    addListener (arr, event ='blur') {
+        arr.forEach((item) => {
+            item.addEventListener(event, this.handleBlur);
+        })
+    },
+    handleBlur () {
+        setTimeout(() => {
+            const scrollHeight = document.documentElement.scrollTop || document.body.scrollTop || 0;
+            window.scrollTo(0, Math.max(scrollHeight - 1, 0));
+        }, 100);
     },
 };
 Controller.init();
