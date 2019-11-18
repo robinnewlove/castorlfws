2fbbf5a54a3684834821734c3ed1738590f9ec9a
Woshiajuana <zhigang.chen@owulia.com>
2019-11-18 15:43:32 +0800

    subcode

---
+++ src/views/20191118/examine/index.js
@@ -0,0 +1,10 @@
+// import $                from 'jquery'
+// import Toast            from 'utils/toast.util'
+
+// 控制器
+const Controller = {
+    init () {
+
+    },
+};
+Controller.init();
---
+++ src/views/20191118/train/entry.js
@@ -0,0 +1,4 @@
+// import 'utils/es6-promise.util'
+import './index'
+import './index.scss'
+
